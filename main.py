39c76399618f8f1bfb37485d10da2c76aad260d7
Latfoo <138173383+Latfoo@users.noreply.github.com>
2026-05-17 20:24:14 +0200

    Add first PostgreSQL integration

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from fastapi import FastAPI, HTTPException
+from sqlmodel import SQLModel, create_engine, Session, Field
+from dotenv import load_dotenv
 
-app = FastAPI()
+from typing import Optional
+from datetime import datetime
+
+import os
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+class PomodoroSession(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    task_label: str
+    duration_minutes: int
+    started_at: datetime = Field(default_factory=datetime.utcnow)
+    completed: bool = False
+
+
+app = FastAPI(title="Pomodoro Backend", description="A backend for a pomodoro timer application")
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
 
 @app.get("/")
 def read_root():
     return {"message": "This is the pomodoro backend!"}
 
+
 @app.get("/health")
 def read_health():
     return {"status": "healthy"}
+
+@app.post("/sessions")
+def create_session(new_session: PomodoroSession):
+    with Session(engine) as db:
+        db.add(new_session)
+        db.commit()
+        db.refresh(new_session)
+        return new_session
